004b226476992aa264d8aa4bed90a7edc56a21dd
AlexandreSouEu <calexandredev@gmail.com>
2019-01-24 11:07:52 -0200

    changing callback to promise

--- 02-node/index.js
+++ 02-node/index.js
@@ -1,6 +1,9 @@
+const util = require('util')
+const addressAsync = util.promisify(getAddress)
+
 function getUser() {
     return new Promise(function userResolve(resolve, reject) {
-        setTimeout(function () {
+        setTimeout(() => {
             //return reject(new Error('DEU RUINZAUM'))
             return resolve({
                 id: 1,
@@ -11,62 +14,58 @@ function getUser() {
     })
 }
 
-function getPhone(idUser, callback) {
-    setTimeout(function () {
-        
-        return callback(null, {
-            phone: '888999',
-            ddd: 55
+function getPhone(idUser) {
+    return new Promise(function phoneResolve(resolve, reject) {
+        setTimeout(() => {
+            return resolve({
+                number: '55555-4444',
+                ddd: 55
+            },2000)  
         })
-
-        
     })
 }
 
 function getAddress(idUser, callback) {
-    setTimeout(function () {
+    setTimeout(() => {
         return callback(null, {
             street: '4 avenue',
             number: 15
+        }, 2000)
+    })
+}
+
+const userPromise = getUser()
+userPromise
+    .then(user => {
+        return getPhone(user.id)
+            .then(resolvePhone = result => {
+                return {
+                    user: {
+                        name: user.name,
+                        id: user.id
+                    },
+                    phone: result
+                }
+            })
+    })
+    .then(result => {
+        const address = addressAsync(result.user.id)
+        return address.then(resolveAddress = resultAddress => {
+            return {
+                user: result.user,
+                phone: result.phone,
+                address: resultAddress
+            
+            }
         })
     })
-}
-
-function userResolve(err, user) {
-    console.log('USER >>>>', user)
-}
-
-const user = getUser()
-user
     .then(result => {
-        console.log('Result', result)
+        console.log(`
+            Name: ${result.user.name},
+            Address: ${result.address.street}, ${result.address.number},
+            Phone: (${result.phone.ddd})${result.phone.number}
+        `)
     })
-    .catch(error => {
+    .catch(function (error) {
         console.log('USER Error!', error)
     })
-/* getUser(function userResolve(err, user) {
-    if (err) {
-        console.error('ERROUUUUU O USER ', err)
-        return
-    }
-
-    getPhone(user.id, function phoneResolve(err1, phone) {
-        if (err1) {
-            console.error('ERROOOU O PHONE ', err1)
-            return
-        }
-        
-        getAddress(user.id, function addressResolve(err2, address) {
-            if (err2) {
-                console.error('ERROU THE ADDRESS: ', err2)
-                return
-            }
-            console.log(`
-              Name: ${user.name},
-              Address: ${address.street},
-              Phone: ${phone.ddd}-${phone.number}
-             `)
-        })
-    })
-
-}) */
