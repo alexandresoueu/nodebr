c8b81c3b5798f466b101db07953d199c8ca0fcce
Alexandre DEV <calexandredev@gmail.com>
2019-01-25 03:36:14 -0200

    Async-Await test Khaue

--- 02-node/async-await.js
+++ 02-node/async-await.js
@@ -1,21 +1,12 @@
-
-async function getUser() {
-    let returnUser
-    const userAsync = await new Promise(function userResolve(resolve, reject) {
-        try {
-            returnUser = setTimeout(() => {
+function getUser() {
+    return new Promise(function userResolve(resolve, reject) {
+        setTimeout(() => {
                 return resolve({
                     id: 1,
                     name: 'Nemo',
                     bornDate: new Date()
                 })
             }, 1000)
-
-        } catch(error) {
-            returnUser = console.log('Errou', error)
-        }
-
-        return userAsync(returnUser)
     })
 }
 
@@ -33,7 +24,7 @@ function getPhone(idUser) {
 function getAddress(idUser) {
     return new Promise(function addressResolve(resolve, reject) {
         setTimeout(() => {
-            return ({
+            return resolve({
                 street: '4 avenue',
                 number: 15
             }, 2000)
@@ -41,37 +32,64 @@ function getAddress(idUser) {
     })
 }
 
-const userPromise = getUser()
-userPromise
-    .then(user => {
-        return getPhone(user.id)
-            .then(resolvePhone = result => {
-                return {
-                    user: {
-                        name: user.name,
-                        id: user.id
-                    },
-                    phone: result
-                }
-            })
-    })
-    .then(result => {
-        return getAddress(result.user.id)
-            .then(resolveAddress = resultAddress => {
-                return {
-                    user: result.user,
-                    phone: result.phone,
-                    address: resultAddress
-                }
-            })
-    })
-    .then(resultAddress => {
+main()
+async function main() {
+    try {
+        console.time('time-await')
+        const user = await getUser()
+        //const phone = await getPhone(user.id)
+        //const address = await getAddress(user.id)
+
+        const resultAll = await Promise.all([
+            getPhone(user.id),
+            getAddress(user.id)
+        ])
+
+        const phone = resultAll[0]
+        const address = resultAll[1]
         console.log(`
-            Name: ${result.user.name},
-            Address: ${result.address.street}, ${result.address.number},
-            Phone: (${result.phone.ddd})${result.phone.number}
+            Name: ${user.name},
+            Phone: (${phone.ddd})${phone.number},
+            Address: ${address.street}, ${address.number}
         `)
-    })
-    .catch(function (error) {
-        console.log('USER Error!', error)
-    })
+        console.timeEnd('time-await')
+    }
+    catch(error) {
+        console.error('CHAPOU O GLOBO..............................', error)
+    }
+}
+
+// const userPromise = getUser()
+// userPromise
+//     .then(user => {
+//         return getPhone(user.id)
+//             .then(resolvePhone = result => {
+//                 return {
+//                     user: {
+//                         name: user.name,
+//                         id: user.id
+//                     },
+//                     phone: result
+//                 }
+//             })
+//     })
+//     .then(result => {
+//         return getAddress(result.user.id)
+//             .then(resolveAddress = result => {
+//                 return {
+//                     user: result.user,
+//                     phone: result.phone,
+//                     address: resultAddress
+//                 }
+//             })
+//     })
+//     .then(result => {
+//         console.log(`
+//             Name: ${result.user.name},
+//             Address: ${result.address.street}, ${result.address.number},
+//             Phone: (${result.phone.ddd})${result.phone.number}
+//         `)
+//     })
+//     .catch(function (error) {
+//         console.log('USER Error!', error)
+//     })
