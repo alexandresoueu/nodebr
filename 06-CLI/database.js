53d7e4831d94ad374e351fa3d4649cc2f1587449
Alexandre DEV <calexandredev@gmail.com>
2019-02-09 22:36:07 -0200

    Create method UPDATE and test

--- 06-CLI/database.js
+++ 06-CLI/database.js
@@ -41,6 +41,27 @@ class Database {
     return dataFilter
   }
 
+  async update(id, changes) {
+    const data = await this.getDataFile()
+    const index = data.findIndex(item => item.id === parseInt(id))
+
+    if(index === -1) {
+      throw Error('The Hero not exist!')
+    }
+
+    const atual = data[index]
+    const objectUpdate = {
+      ...atual,
+      ...changes
+    }
+    data.splice(index, 1)
+
+    return await this.writeFile([
+      ...data,
+      objectUpdate
+    ])
+  }
+
   async remove(id) {
     if(!id) {
       return await this.writeFile([])
--- 06-CLI/test.js
+++ 06-CLI/test.js
@@ -11,9 +11,16 @@ const DEFAULT_ITEM_REGISTER = {
   id: 1
 }
 
+const DEFAULT_ITEM_UPDATE = {
+  name: 'Batman',
+  power: 'Money',
+  id: 2
+}
+
 describe('Handler of Heroes', () => {
   beforeEach(async () => {
     await database.register(DEFAULT_ITEM_REGISTER)
+    //await database.update(DEFAULT_ITEM_UPDATE)
   })
 
   it('Should search a Hero using file', async () => {
@@ -35,6 +42,25 @@ describe('Handler of Heroes', () => {
     const expected = true
     const result = await database.remove(DEFAULT_ITEM_REGISTER.id)
 
+    deepEqual(result, expected)
+  })
+
+  it('Should update a hero for id', async () => {
+    const expected = {
+      ...DEFAULT_ITEM_UPDATE,
+      name: 'Gollum',
+      power: 'Ring'
+    }
+    
+    const newData = {
+      name: 'Gollum',
+      power: 'Ring'
+    }
+
+    await database.update(DEFAULT_ITEM_UPDATE.id, newData)
+    const [result] = await database.list(DEFAULT_ITEM_UPDATE.id)
+
+    console.log('EXPECTED', expected)
     deepEqual(result, expected)
   })
 })
